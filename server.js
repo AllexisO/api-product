14ce1129297a8a0a422074e3b38a5750e4644658
Alexandru Paniciuc <shuricpan@gmail.com>
2025-09-27 01:21:25 +0300

    Port Forwarding

--- server.js
+++ server.js
@@ -275,6 +275,6 @@ app.put('/api/products/:id', async (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
+app.listen(3001, () => {
+    console.log('Server is running on port 3001!');
 });
