b253a20f47a25a22d20679ad73f361aadc96c5ef
Alexandru Paniciuc <shuricpan@gmail.com>
2025-09-30 01:07:27 +0300

    Removed localhost

--- server.js
+++ server.js
@@ -295,6 +295,6 @@ app.delete('/api/products/:id', async (req, res) => {
   });
 });
 
-app.listen(3001, 'localhost', () => {
+app.listen(3001, () => {
     console.log('Server is running on port 3001!');
 });
